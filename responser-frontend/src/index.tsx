4ccb368753f9467abede4a93ad3ca09aa7ea9cd8
Shcherbachenya Igor <evgeniionegin791@gmail.com>
2023-08-30 01:03:35 +0200

    [FE] Optimize fonts loading in pages.

--- responser-frontend/src/index.tsx
+++ responser-frontend/src/index.tsx
@@ -4,6 +4,7 @@ import {Router} from "react-router";
 import {browserHistory} from "router";
 import App from "app/App";
 import "styles/common.less"
+import "styles/fonts.less";
 import 'react-tooltip/dist/react-tooltip.css'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
