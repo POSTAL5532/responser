d87fd1405e09f29070f1db38c555b6eb2822c966
Shcherbachenya Igor <evgeniionegin791@gmail.com>
2023-09-09 22:25:23 +0200

    [EXT] Optimize fonts loading in frames.

--- responser-chrome-extension/src/index.tsx
+++ responser-chrome-extension/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import {Router} from "react-router";
 import {appRouteHistory} from "router";
 import App from "app/App";
+import "styles/fonts.less";
 import "styles/common.less"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
